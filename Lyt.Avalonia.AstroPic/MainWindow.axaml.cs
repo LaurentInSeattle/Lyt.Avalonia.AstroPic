8d7d85bbff63be62892c22548fa42d35fee1e2d2
Laurent <ly.testud@outlook.com>
2025-03-24 13:45:52 -0700

    Empty solution

---
+++ Lyt.Avalonia.AstroPic/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Lyt.Avalonia.AstroPic
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
