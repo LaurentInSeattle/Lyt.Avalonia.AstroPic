7f47cfd82545b2006fdcea06af9872382a1d6f39
Laurent <ly.testud@outlook.com>
2025-03-24 15:40:54 -0700

    doc

--- Lyt.Avalonia.AstroPic.Service/AstroPicService.cs
+++ Lyt.Avalonia.AstroPic.Service/AstroPicService.cs
@@ -1,5 +1,7 @@
 ﻿namespace Lyt.Avalonia.AstroPic.Service;
 
+///  See : https://github.com/nikvoronin/LastWallpaper 
+/// https://github.com/nikvoronin/LastWallpaper?tab=readme-ov-file#media-sources-8 
 public class AstroPicService
 {
 
