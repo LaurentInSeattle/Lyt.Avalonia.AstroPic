f5e36980db34fa8ac15c74bf7787f31e1767caba
Laurent <ly.testud@outlook.com>
2025-04-10 10:29:06 -0700

    Available disk space in stats

--- Lyt.Avalonia.AstroPic/Workflow/Collection/StatisticsViewModel.cs
+++ Lyt.Avalonia.AstroPic/Workflow/Collection/StatisticsViewModel.cs
@@ -20,12 +20,27 @@ public sealed class StatisticsViewModel : Bindable<StatisticsView>
         var statistics = astroPicModel.Statistics;
         this.ImageCountText =
             string.Format("Immagine: {0} (Quota: {1})", statistics.ImageCount, astroPicModel.MaxImages);
-        int sizeOnDisk = (int) ((statistics.SizeOnDiskKB + 512 + 1) / 1024);
+        int sizeOnDisk = (int)((statistics.SizeOnDiskKB + 512 + 1) / 1024);
         this.SizeOnDiskText =
             string.Format("Dimensione stimata su disco: {0} MB (Quota: {1} MB)", sizeOnDisk, astroPicModel.MaxStorageMB);
+        var filemanager = App.GetRequiredService<FileManagerModel>();
+        long availableSpace = filemanager.AvailableFreeSpace(FileManagerModel.Area.User);
+        if (availableSpace > 0)
+        {
+            double availableSpaceGB = availableSpace / (1024.0 * 1024.0 * 1024.0);
+            this.AvailableDiskSpaceText =
+                string.Format("Spazio disponibile su disco: {0:F1} GB", availableSpaceGB);
+        }
+        else
+        {
+            // Could not figure out drive name ? 
+            this.AvailableDiskSpaceText = string.Empty;
+        }
     }
 
     public string ImageCountText { get => this.Get<string>()!; set => this.Set(value); }
 
     public string SizeOnDiskText { get => this.Get<string>()!; set => this.Set(value); }
+
+    public string AvailableDiskSpaceText { get => this.Get<string>()!; set => this.Set(value); }
 }
