f993dce9e64fe8efc4f0a654e4887fb67bf0b6b8
Laurent <ly.testud@outlook.com>
2025-05-17 09:10:19 -0700

    Improved drop view

--- Lyt.Avalonia.AstroPic/Workflow/Collection/DropView.axaml.cs
+++ Lyt.Avalonia.AstroPic/Workflow/Collection/DropView.axaml.cs
@@ -1,22 +1,52 @@
 namespace Lyt.Avalonia.AstroPic.Workflow.Collection;
 
+using static Lyt.Avalonia.Controls.Utilities;
+
 public partial class DropView : UserControl
 {
+    private static SolidColorBrush? normalBrush;
+    private static SolidColorBrush? hotBrush;
+
+    static DropView()
+    {
+        TryFindResource<SolidColorBrush>("LightAqua_0_120", out SolidColorBrush? brush);
+        if (brush is not null)
+        {
+            normalBrush = brush;
+        }
+
+        TryFindResource<SolidColorBrush>("OrangePeel_0_100", out brush);
+        if (brush is not null)
+        {
+            hotBrush = brush;
+        }
+    }
+
     public DropView()
     {
         this.InitializeComponent();
         this.DataContextChanged += this.OnDataContextChanged;
+
+        if (normalBrush is not null)
+        {
+            this.DropRectangle.Stroke = normalBrush;
+        }
+
         DragDrop.SetAllowDrop(this.DropBorder, true);
-        this.AddHandler(DragDrop.DropEvent, this.OnDrop);
+        this.DropBorder.AddHandler(DragDrop.DropEvent, this.OnDrop);
+        this.DropBorder.AddHandler(DragDrop.DragEnterEvent, this.OnDragEnter);
+        this.DropBorder.AddHandler(DragDrop.DragLeaveEvent, this.OnDragLeave);
     }
 
     ~DropView()
     {
         DragDrop.SetAllowDrop(this.DropBorder, false);
         this.DropBorder.RemoveHandler(DragDrop.DropEvent, this.OnDrop);
+        this.DropBorder.RemoveHandler(DragDrop.DragEnterEvent, this.OnDragEnter);
+        this.DropBorder.RemoveHandler(DragDrop.DragLeaveEvent, this.OnDragLeave);
     }
 
-    private void OnDataContextChanged(object? sender, EventArgs e)
+    private void OnDataContextChanged(object? _, EventArgs e)
     {
         if (this.DataContext is DropViewModel dropViewModel)
         {
@@ -24,8 +54,29 @@ public partial class DropView : UserControl
         }
     }
 
-    private void OnDrop(object? sender, DragEventArgs dragEventArgs)
+    private void OnDragEnter(object? _, DragEventArgs e)
     {
+        if (hotBrush is not null)
+        {
+            this.DropRectangle.Stroke = hotBrush;
+        }
+    }
+
+    private void OnDragLeave(object? _, DragEventArgs e)
+    {
+        if (normalBrush is not null)
+        {
+            this.DropRectangle.Stroke = normalBrush;
+        }
+    }
+
+    private void OnDrop(object? _, DragEventArgs dragEventArgs)
+    {
+        if (normalBrush is not null)
+        {
+            this.DropRectangle.Stroke = normalBrush;
+        }
+
         IDataObject data = dragEventArgs.Data;
         var files = data.GetFiles();
         if (files is not null)
